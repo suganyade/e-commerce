5462e52e6e580135bf18e945a0063363c8bd32b8
suganaya devi <suganyadevi067@gmail.com>
2024-02-14 15:08:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var sd = document.querySelector(".side-navbar")
+
+function showbar(event){
+    sd.style.left ="0"
+event.preventDefault()
+}
+function closebar(event){
+    sd.style.left="-60%"
+event.preventDefault()
+}
